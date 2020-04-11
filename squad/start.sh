bcda1c7fb29e47f600569882a159fa408b466ced
AliceNEET <549871272@qq.com>
2020-04-12 02:02:03 +0800

    Correct the start.sh

--- squad/start.sh
+++ squad/start.sh
@@ -22,23 +22,23 @@ if [ ! -f "$eva_file" ]; then
   mv ./data/index.html ./data/evaluate-v2.0.py
 fi
 
-if [ ! -d "./bert-base-cased" ]; then
-  mkdir ./bert-base-cased
+if [ ! -d "./bert-base-uncased" ]; then
+  mkdir ./bert-base-uncased
 fi
 
-wget -i -p ./bert-base-cased/ https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-config.json
-mv ./bert-base-cased/bert-base-uncased-config.json ./bert-base-cased/config.json
-wget -i -p ./bert-base-cased/ https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-vocab.txt
-mv ./bert-base-cased/bert-base-uncased-vocab.txt ./bert-base-cased/vocab.txt
-wget -i -p ./bert-base-cased/ https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-pytorch_model.bin
-mv ./bert-base-cased/bert-base-uncased-pytorch_model.bin ./bert-base-cased/pytorch_model.bin
+wget -i -p ./bert-base-uncased/ https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-config.json
+mv ./bert-base-uncased/bert-base-uncased-config.json ./bert-base-uncased/config.json
+wget -i -p ./bert-base-uncased/ https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-vocab.txt
+mv ./bert-base-uncased/bert-base-uncased-vocab.txt ./bert-base-uncased/vocab.txt
+wget -i -p ./bert-base-uncased/ https://s3.amazonaws.com/models.huggingface.co/bert/bert-base-uncased-pytorch_model.bin
+mv ./bert-base-uncased/bert-base-uncased-pytorch_model.bin ./bert-base-uncased/pytorch_model.bin
 
 export SQUAD_DIR=./data/
-export MODEL_PATH=./bert-base-cased
+export MODEL_PATH=./bert-base-uncased
 
 python run_squad.py \
   --model_type bert \
-  --model_name_or_path $MODEL_PATH \
+  --model_name_or_path bert-base-uncased \
   --do_train \
   --do_eval \
   --version_2_with_negative \
