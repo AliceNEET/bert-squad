fba69aeada7ceb444e4b1f2a92a56d05ebce6caf
AliceNEET <549871272@qq.com>
2020-04-11 11:57:34 +0800

    commit code

---
+++ start.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+train_file = "./data/train-v2.0.json"
+dev_file = "./data/dev-v2.0.json"
+eva_file = "./data/evaluate-v2.0.py"
+
+#如果文件夹不存在，创建文件夹
+if [ ! -d "./data" ]; then
+  mkdir /myfolder
+fi
+
+if [ ! -f "$train_file" ]; then
+  wget -i -p ./data/ https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json
+fi
+
+if [ ! -f "$dev_file" ]; then
+  wget -i -p ./data/ https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json
+fi
+
+if [ ! -f "$eva_file" ]; then
+  wget -i -p ./data/ https://worksheets.codalab.org/rest/bundles/0x6b567e1cf2e041ec80d7098f031c5c9e/contents/blob/
+    mv ./data/index.html ./data/evaluate-v2.0.py
+fi
+
+
+export SQUAD_DIR=./data/
+
+python run_squad.py \
+  --model_type bert \
+  --model_name_or_path bert-base-cased \
+  --do_train \
+  --do_eval \
+  --version_2_with_negative \
+  --do_lower_case \
+  --train_file $SQUAD_DIR/train-v2.0.json \
+  --predict_file $SQUAD_DIR/dev-v2.0.json \
+  --per_gpu_eval_batch_size=12 \
+  --per_gpu_train_batch_size=12 \
+  --learning_rate 3e-5 \
+  --num_train_epochs 4.0 \
+  --max_seq_length 384 \
+  --doc_stride 128 \
+  --save_steps 5000\
+  --output_dir ./output/
