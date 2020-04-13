9250246ce1b4588b10f04c5682541d2ddf50323f
AliceNEET <549871272@qq.com>
2020-04-14 01:50:22 +0800

    Modify the start.sh using pretrain_model

--- squad/start.sh
+++ squad/start.sh
@@ -7,9 +7,9 @@ eva_file = "./data/evaluate-v2.0.py"
 #如果文件夹不存在，创建文件夹
 mkdir data
 
-wget -ip ./data/ https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json
-wget -ip ./data/ https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json
-wget -ip ./data/ https://worksheets.codalab.org/rest/bundles/0x6b567e1cf2e041ec80d7098f031c5c9e/contents/blob/
+wget -ip ./data https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json
+wget -ip ./data https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json
+wget -ip ./data https://worksheets.codalab.org/rest/bundles/0x6b567e1cf2e041ec80d7098f031c5c9e/contents/blob/
 mv ./data/index.html ./data/evaluate-v2.0.py
 
 export SQUAD_DIR=./data/
