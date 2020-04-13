2ed495b41ab81a208ea9287ecb69aed61830e06e
AliceNEET <549871272@qq.com>
2020-04-14 01:41:51 +0800

    Modify the start.sh using pretrain_model

--- squad/start.sh
+++ squad/start.sh
@@ -5,26 +5,15 @@ dev_file = "./data/dev-v2.0.json"
 eva_file = "./data/evaluate-v2.0.py"
 
 #如果文件夹不存在，创建文件夹
-if [ ! -d "./data" ]; then
-  mkdir ./data
-fi
+mkdir data
 
-if [ ! -f "$train_file" ]; then
-  wget -ip ./data/ https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json
-fi
-
-if [ ! -f "$dev_file" ]; then
-  wget -ip ./data/ https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json
-fi
-
-if [ ! -f "$eva_file" ]; then
-  wget -ip ./data/ https://worksheets.codalab.org/rest/bundles/0x6b567e1cf2e041ec80d7098f031c5c9e/contents/blob/
-  mv ./data/index.html ./data/evaluate-v2.0.py
-fi
-
+wget -ip ./data/ https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json
+wget -ip ./data/ https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json
+wget -ip ./data/ https://worksheets.codalab.org/rest/bundles/0x6b567e1cf2e041ec80d7098f031c5c9e/contents/blob/
+mv ./data/index.html ./data/evaluate-v2.0.py
 
 export SQUAD_DIR=./data/
-export MODEL_PATH=/pretrain_model/bert-base-uncased
+export MODEL_PATH=/dataset/bert-base-uncased
 
 python run_squad.py \
   --model_type bert \
