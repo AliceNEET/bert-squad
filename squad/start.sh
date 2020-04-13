707bae2d14827a9fc3e2ae7a4e9e37c73d8fb233
AliceNEET <549871272@qq.com>
2020-04-14 02:04:07 +0800

    Modify the start.sh using pretrain_model

--- squad/start.sh
+++ squad/start.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
-
-train_file = "./data/train-v2.0.json"
-dev_file = "./data/dev-v2.0.json"
-eva_file = "./data/evaluate-v2.0.py"
-
-#如果文件夹不存在，创建文件夹
-mkdir data
-
-wget -ip ./data/ https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json
-wget -ip ./data/ https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json
-wget -ip ./data/ https://worksheets.codalab.org/rest/bundles/0x6b567e1cf2e041ec80d7098f031c5c9e/contents/blob/
-mv ./data/index.html ./data/evaluate-v2.0.py
-
 export SQUAD_DIR=./data
 export MODEL_PATH=/dataset/bert-base-uncased
 
